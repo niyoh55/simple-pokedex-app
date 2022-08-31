4efffcc51b1758bc84353bacb732ea2be7a3d3d9
niyoh55 <niyohedwyn21@gmail.com>
2022-08-31 12:48:45 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex flex-row justify-center items-center h-screen w-screen bg-pink-200">
+      <h1 className="text-9xl">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
